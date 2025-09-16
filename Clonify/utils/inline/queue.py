8373a6a5a275abf2d1e0c2578f0e1d4d080efbde
AVENGERSOWNER1 <akgameing452@gmail.com>
2025-09-16 23:01:13 +0545

    Update queue.py

--- Clonify/utils/inline/queue.py
+++ Clonify/utils/inline/queue.py
@@ -75,10 +75,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/KRITI_UPDATE"
+                text="˹❍ᴡηєʀ ᴀʙσᴜᴛ˼ ", url=f"https://t.me/AvengersNetwork"
             ),
             InlineKeyboardButton(
-                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/TEAM_BADNAM_BOTS"
+                text="˹ᴄʜᴧᴛ ɢʀσᴜᴘ˼", url=f"https://t.me/+ijVUQc7TNEUwNmNl"
             ),
         ],
       ]
@@ -113,7 +113,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/ARISHFA_UPDATE",
+                url="https://t.me/AvengersNetwork",
             ),
         ],
     ]

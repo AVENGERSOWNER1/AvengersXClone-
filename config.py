bc012869d2f9fb1b59f81e35dfe3bec044b35df5
UTTAM RATHORE <uk977061@gmail.com>
2025-10-11 19:03:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ ASSUSERNAME = getenv("ASSUSERNAME" , "Sonali")
 #---------------------------------------------------------------
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 API_KEY = getenv("API_KEY")
+VIDEO_API_URL = getenv("VIDEO_API_URL")
 BABYAPI = f"http://152.42.189.60:1470/song?query={vidid}"
 #---------------------------------------------------------------
 #---------------------------------------------------------------
